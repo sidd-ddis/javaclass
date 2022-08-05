9db4bb37191d5413d33092ac261c000f143f89d4
Sid <Siddhart8525@gmail.com>
2022-08-05 14:27:06 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+ class student {
+     int id = 01;
+     String name = "siddharth";
+     int standard = 12;
+}
+ class student1 {
+    public static void main(String[] args) {
+        student s1= new student();
+        System.out.println(s1.id + s1.name + s1.standard);
+
+        System.out.println("maza aa rha h ....?");
+    }
+}
